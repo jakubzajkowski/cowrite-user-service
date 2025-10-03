eccbdee367514381c43f54f037582556547e86b3
jakubzajkowski <jakudjan123@gmail.com>
2025-10-03 16:46:14 +0200

    feat: implement configuration of flyway

--- src/main/java/com/example/cowrite/entity/User.java
+++ src/main/java/com/example/cowrite/entity/User.java
@@ -18,12 +18,35 @@ public class User {
     @Column(nullable = false, unique = true)
     private String username;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
